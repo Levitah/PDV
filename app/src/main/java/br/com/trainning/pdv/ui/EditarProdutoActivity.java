d521aca70e274c77ed577dd075b794d25e81a3c4
android <giovannifelipeguilherme@hotmail.com>
2016-03-12 10:42:32 -0300

    - Atualizações até 10h42;

--- app/src/main/java/br/com/trainning/pdv/ui/EditarProdutoActivity.java
+++ app/src/main/java/br/com/trainning/pdv/ui/EditarProdutoActivity.java
@@ -1,6 +1,11 @@
 package br.com.trainning.pdv.ui;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,20 +14,46 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import br.com.trainning.pdv.R;
 import br.com.trainning.pdv.domain.model.Produto;
+import br.com.trainning.pdv.domain.util.Base64Util;
+import br.com.trainning.pdv.domain.util.ImageInputHelper;
 import butterknife.Bind;
+import butterknife.OnClick;
 import se.emilsjolander.sprinkles.Query;
 
-public class EditarProdutoActivity extends BaseActivity {
+public class EditarProdutoActivity extends BaseActivity implements ImageInputHelper.ImageActionListener {
 
     @Bind(R.id.spinner)
     Spinner spinner;
+    @Bind(R.id.editTextDescricao)
+    EditText editTextDescricao;
+    @Bind(R.id.editTextUnidade)
+    EditText editTextUnidade;
+    @Bind(R.id.editTextPreco)
+    EditText editTextPreco;
+    @Bind(R.id.editTextCodigoBarras)
+    EditText editTextCodigoBarras;
+
+    @Bind(R.id.imageViewFoto)
+    ImageView imageViewFoto;
+    @Bind(R.id.imageButtonCamera)
+    ImageButton imageButtonCamera;
+    @Bind(R.id.imageButtonGaleria)
+    ImageButton ImageButtonGaleria;
+
+    private ImageInputHelper imageInputHelper;
+    private Produto produto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +62,22 @@ public class EditarProdutoActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        imageInputHelper = new ImageInputHelper(this);
+        imageInputHelper.setImageActionListener(this);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                produto.setDescricao(editTextDescricao.getText().toString());
+                produto.setUnidade(editTextUnidade.getText().toString());
+                produto.setCodigoBarras(editTextCodigoBarras.getText().toString());
+                if (!editTextPreco.getText().toString().trim().equals(""))
+                    produto.setPreco(Double.parseDouble(editTextPreco.getText().toString().toString()));
+                produto.setFoto(Base64Util.encodeTobase64(((BitmapDrawable)imageViewFoto.getDrawable()).getBitmap()));
+                produto.save();
+
+                Snackbar.make(view, "Produto alterado com sucesso!", Snackbar.LENGTH_SHORT).show();
             }
         });
 
@@ -62,6 +103,15 @@ public class EditarProdutoActivity extends BaseActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String barCode = parent.getItemAtPosition(position).toString();
                 Log.d("BARCODE", "selecionado~~>" + barCode);
+
+                produto = Query.one(Produto.class, "select * from produto where codigo_barra = ?", barCode).get();
+                if (produto != null) {
+                    editTextDescricao.setText(produto.getDescricao());
+                    editTextUnidade.setText(produto.getUnidade());
+                    editTextPreco.setText(String.valueOf(produto.getPreco()));
+                    editTextCodigoBarras.setText(produto.getCodigoBarras());
+                    imageViewFoto.setImageBitmap(Base64Util.decodeBase64(produto.getFoto()));
+                }
             }
 
             @Override
@@ -69,5 +119,47 @@ public class EditarProdutoActivity extends BaseActivity {
 
             }
         });
+    }
+
+    @OnClick(R.id.imageButtonGaleria)
+    public void onClickGaleria() {
+        imageInputHelper.selectImageFromGallery();
+    }
+
+    @OnClick(R.id.imageButtonCamera)
+    public void onClickCamera() {
+        imageInputHelper.takePhotoWithCamera();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        imageInputHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onImageSelectedFromGallery(Uri uri, File imageFile) {
+        // cropping the selected image. crop intent will have aspect ratio 16/9 and result image
+        // will have size 800x450
+        imageInputHelper.requestCropImage(uri, 100, 100, 0, 0);
+    }
+
+    @Override
+    public void onImageTakenFromCamera(Uri uri, File imageFile) {
+        // cropping the taken photo. crop intent will have aspect ratio 16/9 and result image
+        // will have size 800x450
+        imageInputHelper.requestCropImage(uri, 100, 100, 0, 0);
+    }
+
+    @Override
+    public void onImageCropped(Uri uri, File imageFile) {
+        try {
+            // getting bitmap from uri
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            imageViewFoto.setImageBitmap(bitmap);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
